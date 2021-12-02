3e9ac1e720014c8eebcd1f789b4de6fc89bcfea1
Daniel Laera <daniel.laera@ifchor.com>
2021-12-02 21:17:14 +0000

    sticky buttons & miscellaneous

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -3,8 +3,9 @@ import { MDXRemote, MDXRemoteSerializeResult } from 'next-mdx-remote';
 import { serialize } from 'next-mdx-remote/serialize';
 import dynamic from 'next/dynamic';
 import Head from 'next/head';
+import Link from "next/link";
 import { useRouter } from 'next/router';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Ingredients from '../../components/Ingredients';
 import Instructions from '../../components/Instructions';
 import Layout from '../../components/Layout';
@@ -25,13 +26,36 @@ const components = {
   Tips: dynamic(() => import('../../components/Tips'))
 };
 
+export function useQuery() {
+  const router = useRouter();
+
+  const hasQueryParams = /\[.+\]/.test(router.route) || /\?./.test(router.asPath);
+  const ready = !hasQueryParams || Object.keys(router.query).length > 0;
+
+  if (!ready) return null;
+
+  return router.query;
+}
+
 const PostPage: React.FC<Props> = ({ source, frontMatter }: Props) => {
+  const [isVisible, setIsVisible] = useState(false);
+  const [isOpacity, setOpacity] = useState(false);
   const router = useRouter();
   const lang = router.locale;
 
   const ogImage = SITE_URL + frontMatter.thumbnail;
 
   const { setLang, setIngredients, setInstructions, setTips } = useMdxComponentsContext();
+
+  const query = useQuery();
+
+  useEffect(() => {
+    if (!query) {
+      return;
+    }
+    // fetch the dynamic page routeId
+    console.log('my query exists!!', query.slug);
+  }, [query]);
 
   useEffect(() => {
     setLang(lang);
@@ -49,31 +73,113 @@ const PostPage: React.FC<Props> = ({ source, frontMatter }: Props) => {
     setTips
   ]);
 
+  // Top: 0 takes us all the way back to the top of the page
+  // Behavior: smooth keeps it smooth!
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  };
+
+  useEffect(() => {
+    // Button is displayed after scrolling for 500 pixels
+    const toggleVisibility = () => {
+      if (window.pageYOffset > 500) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+
+    const toggleOpacity = () => {
+      if (window.pageYOffset > 50) {
+        setOpacity(true);
+      } else {
+        setOpacity(false);
+      }
+    };
+
+    window.addEventListener("scroll", toggleVisibility);
+    window.addEventListener("scroll", toggleOpacity);
+
+    return () => {
+      window.removeEventListener("scroll", toggleVisibility),
+        window.removeEventListener("scroll", toggleOpacity)
+    };
+  }, [])
+
+
+
   return (
-    <Layout pageTitle={frontMatter.title}>
-      <Head>
-        <meta name="description" content={frontMatter.description} key="description" />
-        <meta property="og:description" content={frontMatter.description} key="ogDescription" />
-        <meta property="og:image" content={ogImage} key="ogImage" />
-      </Head>
+    <>
+      <Layout pageTitle={frontMatter.title}>
+        <Head>
+          <meta name="description" content={frontMatter.description} key="description" />
+          <meta property="og:description" content={frontMatter.description} key="ogDescription" />
+          <meta property="og:image" content={ogImage} key="ogImage" />
+        </Head>
 
-      <article className="prose prose-green dark:prose-dark">
-        <div className="mb-4">
-          <Thumbnail title={frontMatter.title} src={frontMatter.thumbnail} />
-        </div>
+        <Link passHref
+          href={{
+            pathname: "/"
+          }}
+        >
+          <nav
+            id="my-breacrumb"
+            className={`
+            ${isOpacity ? 'bg-opacity-20' : 'bg-opacity-0'}
+          mb-4 sticky top-1
+    block
+    text-sm text-left text-green-600
+    bg-green-500 
+    h-12
+    flex
+    items-center
+    p-4
+    rounded-md
+  `}
+            role="alert"
+          >
+            <ol className="list-reset flex text-grey-dark">
+              <li><a href="#" className="font-bold">Home</a></li>
+              <li><span className="mx-2">/</span></li>
+              <li><a href="#" className="font-bold">{query.slug}</a></li>
+            </ol>
+          </nav>
+        </Link>
 
-        <h1>{frontMatter.title}</h1>
+        <article className="prose prose-green dark:prose-dark">
+          <div className="mb-4">
+            <Thumbnail title={frontMatter.title} src={frontMatter.thumbnail} />
+          </div>
 
-        <p className="font-bold">
-          {lang === 'fr' ? 'les yieldés' : 'Yields: '}
-          {frontMatter.yields}
-        </p>
+          <h1>{frontMatter.title}</h1>
 
-        <p>{frontMatter.description}</p>
+          <p className="font-bold">
+            {lang === 'fr' ? 'les yieldés' : 'Yields: '}
+            {frontMatter.yields}
+          </p>
 
-        <MDXRemote {...source} components={components} />
-      </article>
-    </Layout>
+          <p>{frontMatter.description}</p>
+
+          <MDXRemote {...source} components={components} />
+
+        </article>
+
+      </Layout>
+
+      <div className="fixed bg-green-500 bottom-3 right-3  cursor-pointer">
+        {isVisible && (
+          <div
+            onClick={scrollToTop}
+            className="fixed p-2 rounded-lg bg-green-500 bottom-3 right-3 lg:bottom-5 lg:right-5 cursor-pointer"
+          >
+            <h3>Go up!</h3>
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 
